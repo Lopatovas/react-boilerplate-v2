b0d9bb2f279a8eaebab2cd11a77c2da023309c36
Edgaras Lopatovas <Lopatovas@gmail.com>
2021-01-20 18:30:02 +0200

    Add proper css loader

--- src/containers/Landing/landing.tsx
+++ src/containers/Landing/landing.tsx
@@ -5,6 +5,9 @@ const Landing = () => {
     return (
       <div className="container">
           <Dummy text={"React Boilerplate V2"}/>
+          <p>Typescript</p>
+          <p>React-Router-Dom</p>
+          <p>CSS</p>
       </div>
     );
 }
